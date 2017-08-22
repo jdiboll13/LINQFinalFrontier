59a7ffa6ffadfc2c37105c99a2ab56825428b7ff
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-22 15:17:58 -0400

    all the things work, but no error handling. use at own risk.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQFinalFrontier
 {
@@ -7,27 +8,53 @@ namespace LINQFinalFrontier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var spaceships = new List<Spaceship>();
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            spaceships.Add(new Spaceship());
+            Console.WriteLine(sumOfEarthShips(spaceships));
+            sendShipsToFightMonsters(spaceships);
+            foreach (var item in ExplorationFleet(spaceships))
+            {
+                Console.WriteLine(item.Name);
+            }
+            
+            Console.WriteLine(findMostGunsWithSize(spaceships, 15).Name);
+
         }
         //In Program.cs Write these functions using functional programming:
         public static int sumOfEarthShips(List<Spaceship> ships)
         {
-            return -1;
+            var earthShips = ships.Where(w => w.HasWarpDrive==false).Select(s => s.Name).Count();
+            return earthShips;
         }
 
         public static void sendShipsToFightMonsters(List<Spaceship> ships)
         {
-
+            var fightingShips = ships.Where(w => w.HasWarpDrive).ToList();
+            foreach (Spaceship item in fightingShips)
+            {
+                item.fightSpaceMonster();
+            }
         }
 
         public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size)
         {
-            return null;
+            var fightingShips = ships.Where(w => w.Size == size).OrderByDescending(o => o.NumLasers).First();
+            return fightingShips;
         }
 
         public static IEnumerable<Spaceship> ExplorationFleet(List<Spaceship> ships)
         {
-            return null;
+            var reconShips = ships.Where(w => w.HasWarpDrive).OrderBy(o => o.NumLasers).Take(5).ToList();
+            return reconShips;
         }
     }
 
--- Spaceship.cs
+++ Spaceship.cs
@@ -4,48 +4,53 @@ using System.Linq;
 
 namespace LINQFinalFrontier
 {
-    public class Spaceship 
+    public class Spaceship
     {
 
-        public int NumLasers {get;set;}
-        public int Size {get;set;}
-        public bool HasWarpDrive {get;set;}
-        public String Name {get;set;}
-
+        public int NumLasers { get; set; }
+        public int Size { get; set; }
+        public bool HasWarpDrive { get; set; }
+        public String Name { get; set; }
+        static int nameCounter = 0;
         private static String[] names = {"Enterprise", "Voyager", "Wuddship", "Ariel", "Pegasus",
                 "Defiant", "Valiant", "Victory", "Trafalgar", "Shadow"};
 
-        public Spaceship (int numLasers, int size, bool hasWarpDrive, String name) {
+        public Spaceship(int numLasers, int size, bool hasWarpDrive, String name)
+        {
             this.NumLasers = numLasers;
             this.Size = size;
             this.HasWarpDrive = hasWarpDrive;
             this.Name = name;
         }
 
-        public Spaceship () {
+        public Spaceship()
+        {
             var r = new Random();
-            var nameCounter = 0;
 
-            this.NumLasers = r.Next(0,20);
-            this.Size = r.Next(0,15);
-            this.HasWarpDrive = r.Next(0,100) % 2 == 0;;
-
+            this.NumLasers = r.Next(0, 20);
+            this.Size = r.Next(0, 15);
+            this.HasWarpDrive = r.Next(0, 100) % 2 == 0; ;
 
             this.Name = names[nameCounter];
             nameCounter++;
         }
 
-        public void fightSpaceMonster () {
+        public void fightSpaceMonster()
+        {
             Random random = new Random();
-            int spaceMonsterHealth = random.Next(0,12);
-            if (this.NumLasers >= spaceMonsterHealth) {
+            int spaceMonsterHealth = random.Next(0, 12);
+            if (this.NumLasers >= spaceMonsterHealth)
+            {
                 Console.WriteLine(Name + " wins the fight.");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine(Name + " lost the fight.");
             }
         }
 
-        public String toString () {
+        public String toString()
+        {
             return $"Name: {Name} \nLasers: {NumLasers} \nSize: {Size} \nWarpDrive: {HasWarpDrive}";
         }
 
