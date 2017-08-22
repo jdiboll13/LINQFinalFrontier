c4750eb7cbef7ae0cd5e87d0e9b279d6d84e6e30
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-22 13:37:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQFinalFrontier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //In Program.cs Write these functions using functional programming:
+        public static int sumOfEarthShips(List<Spaceship> ships)
+        {
+            return -1;
+        }
+
+        public static void sendShipsToFightMonsters(List<Spaceship> ships)
+        {
+
+        }
+
+        public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size)
+        {
+            return null;
+        }
+
+        public static IEnumerable<Spaceship> ExplorationFleet(List<Spaceship> ships)
+        {
+            return null;
+        }
+    }
+
+
+}
+
---
+++ Spaceship.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQFinalFrontier
+{
+    public class Spaceship 
+    {
+
+        public int NumLasers {get;set;}
+        public int Size {get;set;}
+        public bool HasWarpDrive {get;set;}
+        public String Name {get;set;}
+
+        private static String[] names = {"Enterprise", "Voyager", "Wuddship", "Ariel", "Pegasus",
+                "Defiant", "Valiant", "Victory", "Trafalgar", "Shadow"};
+
+        public Spaceship (int numLasers, int size, bool hasWarpDrive, String name) {
+            this.NumLasers = numLasers;
+            this.Size = size;
+            this.HasWarpDrive = hasWarpDrive;
+            this.Name = name;
+        }
+
+        public Spaceship () {
+            var r = new Random();
+            var nameCounter = 0;
+
+            this.NumLasers = r.Next(0,20);
+            this.Size = r.Next(0,15);
+            this.HasWarpDrive = r.Next(0,100) % 2 == 0;;
+
+
+            this.Name = names[nameCounter];
+            nameCounter++;
+        }
+
+        public void fightSpaceMonster () {
+            Random random = new Random();
+            int spaceMonsterHealth = random.Next(0,12);
+            if (this.NumLasers >= spaceMonsterHealth) {
+                Console.WriteLine(Name + " wins the fight.");
+            } else {
+                Console.WriteLine(Name + " lost the fight.");
+            }
+        }
+
+        public String toString () {
+            return $"Name: {Name} \nLasers: {NumLasers} \nSize: {Size} \nWarpDrive: {HasWarpDrive}";
+        }
+
+    }
+}
